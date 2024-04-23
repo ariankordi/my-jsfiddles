e430dd8d9975c4496fc39cf86017e3a3d7a8c364
Arian Kordi <ariankordi@ariankordi.net>
2024-04-23 00:24:39 +0000

    old-mii2studio-attempt-2019: revision 4
    Fiddle-Id: zj9gnm2x
    Fiddle-Revision: 4

--- old-mii2studio-attempt-2019/script.js
+++ old-mii2studio-attempt-2019/script.js
@@ -22,10 +22,10 @@ function miiMap2Studio(map) {
 function u8Mii2Map(u8) {
 	// u8 is a Uint8Array that shouldn't be modified
   return [
-  	// unknown?
-  	8,
+  	// beard color
+  	(u8[0x42] >> 0x03) & 0x07,
     // beard type (0 for none)
-    0,
+    u8[0x42] & 0x07,
     // fatness
     u8[0x2f],
     // eye thickness
@@ -51,7 +51,7 @@ function u8Mii2Map(u8) {
     // eyebrow scale
     u8[0x39] & 0x07,
     // eyebrow type
-    u8[0x38] & 0x05,
+    u8[0x38] & 0x1f,
     // eyebrow distance
     (u8[0x3b] & 0x01) + (u8[0x3a] >> 0x05),
     // eyebrow height
@@ -59,7 +59,7 @@ function u8Mii2Map(u8) {
     // face color (!)
     u8[0x30] >> 0x05,
     // blush type (0 for none)
-    u8[0x30] >> 0x04,
+    u8[0x31] >> 0x04,
     // face type
     (0x0e & u8[0x30]) >> 0x01,
     // face style (wrinkles) (0 for none)
@@ -68,14 +68,14 @@ function u8Mii2Map(u8) {
     (0x3c & u8[0x19]) >> 0x02,
     // male/female
     u8[0x18] & 0x01,
-    // unknown?
-    8,
-    // unknown?
+    // glasses color
+    2,
+    // glasses scale
     4,
     // glasses type (0 for none)
-    0,
+    u8[0x44] & 0x0f,
     // glasses height
-    10,
+    17,
     // hair color (!)
     u8[0x33] & 0x06,
     // hair flipped
@@ -84,8 +84,8 @@ function u8Mii2Map(u8) {
     u8[0x32],
     // height
     u8[0x2e],
-    // unknown?
-    4,
+    // mole scale
+    5,
     // mole enabled
     0,
     // mole x
@@ -95,18 +95,18 @@ function u8Mii2Map(u8) {
     // mouth thickness
     u8[0x3f] >> 5,
     // unknown?
-    19,
+    4,
     // mouth size
     (0x0e & u8[0x3f]) >> 0x01,
     // mouth type
     u8[0x3e] & 0x3f,
     // mouth height
     u8[0x40] & 0x1f,
-    // unknown?
-    4,
-    // moustache type (0 for none)
+    // mustache scale
+    u8[0x44] >> 0x07,
+    // mustache type (0 for none)
     0,
-    // moustache height
+    // mustache height
     10,
     // nose scale
     (u8[0x3b] & 0x01) + (u8[0x3c] >> 0x05),
@@ -129,13 +129,56 @@ function b642u8(str) {
 }
 
 // do stuff
+var miiPreview = document.getElementById('mii-preview');
+
+let currentMiiB64 = 'AwEAQOiHu+XgBGBQ1wSZbRghyJwF/wAAWRhQAEYAMgBHAAAAUABGADIATQAAAH8uCAAMBrtqQhThI2UOYRIIRA4AIEEAUhAdAABhAG4AZQAAAAAAAAAAAAAAAAAAAKz5';
+let currentMiiMap = u8Mii2Map(b642u8(currentMiiB64));
+let currentMiiStudioValue = miiMap2Studio(currentMiiMap);
+let currentMiiStudioURL = 'https://studio.mii.nintendo.com/miis/image.png?type=all_body&width=512&data=' + currentMiiStudioValue;
+miiPreview.setAttribute('src', currentMiiStudioURL);
+
+var miiSubmit = document.getElementById('mii-submit');
+var miiInput = document.getElementById('mii-input');
+var submitButton = document.getElementById('submit-button');
+miiSubmit.addEventListener('submit', event => {
+	event.preventDefault();
+  if(currentMiiB64 == miiInput.value || miiInput.value == '') {
+  	return;
+  }
+  submitButton.setAttribute('disabled', '');
+  currentMiiB64 = miiInput.value;
+  if(currentMiiB64.indexOf('-') > -1 || currentMiiB64.indexOf('_') > -1) {
+  	currentMiiB64 = currentMiiB64.replace('-', '+').replace('_', '/')
+  }
+  try {
+  	currentMiiMap = u8Mii2Map(b642u8(currentMiiB64));
+  } catch(e) {
+  	console.log(e)
+  	submitButton.removeAttribute('disabled');
+  	return;
+  }
+  currentMiiStudioValue = miiMap2Studio(currentMiiMap);
+  miiPreview.addEventListener('load', () => {
+  	submitButton.removeAttribute('disabled');
+  });
+  miiPreview.addEventListener('error', () => {
+  	submitButton.removeAttribute('disabled');
+    miiPreview.setAttribute('src', '');
+  });
+  currentMiiStudioURL = 'https://studio.mii.nintendo.com/miis/image.png?type=all_body&width=512&data=' + currentMiiStudioValue;
+  miiPreview.setAttribute('src', currentMiiStudioURL);
+});
+
+
 // convert base64 to u8
+//const newMiiMap = u8Mii2Map(b642u8('AAAAQGqGu0VgRPAw0saNTjSvLJz6OwAArkJyAGUAbgBlAGYAdQB6AHoAeQAAAEBACAAiAVpk4hQsIsQMsAwTZg0ACCWBQUhQQgBlAHIAdABpAAAAAAAAAAAAAAAAAAXk'));
+//const newMiiMap = u8Mii2Map(b642u8('AwAAQArm+6WgxBAw1fMhvS/6E5zKTgAAClJWAGkAbgBlAHMAYQB1AGMAZQAAAGIoBLhXAVFnZBYuEoUSwRAPZg4AACkAUkhQAAByACAAbQB1AG0AIAB4AGQAZAAAAD1d'));
 //const newMiiMap = u8Mii2Map(b642u8('AwAAMKtmz7kOsnHolemSncz7ZfBbsAAAAWBUAGEAbgB1AGsAaQBEAGUAZQAAADcjAABpAa1oYxggNEYWgRJPaA0AACkgYkhQAAAAAAAAAAAAAAAAAAAAAAAAAAAAABnp'));
 //const newMiiMap = u8Mii2Map(b642u8('AwAAMLYhkwbv2enqkdzbyFi9o+5zsgAAk2qGMAAAWDAxAGsAMQAyAGQAVTCTMFFDEhBlBhJkpBzAYGYWAgrCBAYAAClAABAAhjBEMAAAAAAAAAAAAAAAAAAAAAAAAAnv'));
 //const newMiiMap = u8Mii2Map(b642u8('AwEAMIAhWGSARACgkc32dOAMf+R8aQAAWFhQAEYAMgBNAAAAUABGADIATQAAAH8uCAAzBqUoQxLhI2UOgRIVZg4AIEEAUhAdTABhAG4AZQAAAAAAAAAAAAAAAAAAAPln'));
-const newMiiMap = u8Mii2Map(b642u8('AwEAQOiHu+XgBGBQ1wSZbRghyJwF/wAAWRhQAEYAMgBHAAAAUABGADIATQAAAH8uCAAMBrtqQhThI2UOYRIIRA4AIEEAUhAdAABhAG4AZQAAAAAAAAAAAAAAAAAAAKz5'));
-const newMiiStudioValue = miiMap2Studio(newMiiMap);
+//const newMiiMap = u8Mii2Map(b642u8('AwEAQOiHu+XgBGBQ1wSZbRghyJwF/wAAWRhQAEYAMgBHAAAAUABGADIATQAAAH8uCAAMBrtqQhThI2UOYRIIRA4AIEEAUhAdAABhAG4AZQAAAAAAAAAAAAAAAAAAAKz5'));
+//const newMiiStudioValue = miiMap2Studio(newMiiMap);
 // here is the actual mii studio image url
-const miiStudioURL = 'https://studio.mii.nintendo.com/miis/image.png?type=all_body&width=512&data=' + newMiiStudioValue;
+//const miiStudioURL = 'https://studio.mii.nintendo.com/miis/image.png?type=all_body&width=512&data=' + newMiiStudioValue;
 // update img
-document.getElementById('mii-preview').setAttribute('src', miiStudioURL);
+//document.getElementById('mii-preview').setAttribute('src', miiStudioURL);

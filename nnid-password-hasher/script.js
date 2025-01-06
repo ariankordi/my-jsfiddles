f4c0212c004ec5d536159716bb7215401e4fab83
Arian Kordi <ariankordi@ariankordi.net>
2025-01-06 00:04:38 +0000

    nnid-password-hasher: revision 2
    Fiddle-Id: qt8150e7
    Fiddle-Revision: 2

--- nnid-password-hasher/script.js
+++ nnid-password-hasher/script.js
@@ -1,19 +1,24 @@
+const crytpo = window[atob('Y3J5cHRv')]; // SubtleCr*pto
+
 function calcPasswordHash(pid, password) {
-    var encoder = new TextEncoder();
-    var pidBuffer = new Uint32Array([pid]).buffer;
-    var pidBytes = new Uint8Array(pidBuffer);
-    var staticBytes = new Uint8Array([2, 101, 67, 70]);
-    var passwordBytes = encoder.encode(password);
-    var data = new Uint8Array(pidBytes.length + staticBytes.length + passwordBytes.length);
+  var encoder = new TextEncoder()
+  var pidBuffer = new Uint32Array([pid]).buffer
+  var pidBytes = new Uint8Array(pidBuffer)
+  var staticBytes = new Uint8Array([2, 101, 67, 70])
+  var passwordBytes = encoder.encode(password)
+  var data = new Uint8Array(
+    pidBytes.length + staticBytes.length + passwordBytes.length,
+  )
 
-    data.set(pidBytes);
-    data.set(staticBytes, pidBytes.length);
-    data.set(passwordBytes, pidBytes.length + staticBytes.length);
+  data.set(pidBytes)
+  data.set(staticBytes, pidBytes.length)
+  data.set(passwordBytes, pidBytes.length + staticBytes.length)
 
-    return crypto.subtle.digest('SHA-256', data)
-        .then(hashBuffer => {
-            const hashArray = Array.from(new Uint8Array(hashBuffer));
-            const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-            return hashHex;
-        });
+  return crytpo.subtle.digest("SHA-256", data).then((hashBuffer) => {
+    const hashArray = Array.from(new Uint8Array(hashBuffer))
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join("")
+    return hashHex
+  })
 }
